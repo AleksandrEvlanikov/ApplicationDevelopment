d6d56d59e916c448b86059b87d883479c276787e
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-10-14 17:12:49 +0300

    Add project files.

---
+++ ApplicationDevelopment/Indentation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopment
+{
+    public class Indentation
+    {
+        public string indentation10 { get; } = new string(' ', 10);
+        public string indentation20 { get; } = new string(' ', 20);
+        public string indentation40 { get; }  = new string(' ', 40);
+        public string indentation60 { get; } = new string(' ', 60);
+        public string indentation80 { get; } = new string(' ', 80);
+
+    }
+}
