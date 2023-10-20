fffc22c4493e550d3007b1f7f5dbfb7e1b3eee3b
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-10-20 17:13:31 +0300

    Dz1

---
+++ ApplicationDevelopment/Dz1/Child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopment.NewFolder
+{
+    public class Child : FamilyMember
+    {
+        public Child(string name, Gender gender, FamilyMember parents) : base(name, gender, parents)
+        {
+
+        }
+    }
+}
