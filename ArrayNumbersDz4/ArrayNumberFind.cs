bd3ce6e058700fb12079deb9eee9216cd781abec
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-10-25 15:00:18 +0300

    Dz4

---
+++ ArrayNumbersDz4/ArrayNumberFind.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNumbersDz4
+{
+    //Дан массив и число.Найдите три числа в массиве сумма которых равна искомому числу.Подсказка: если взять первое число в массиве, 
+    //    можно ли найти в оставшейся его части два числа равных по сумме первому.
+    public class ArrayNumberFind
+    {
+        public void FindTarget(int target, int[] arr)
+        {
+
+            HashSet<int> setArr = new HashSet<int>();
+
+            int theeArr;
+            foreach (var item0 in arr)
+            {
+                foreach (var item1 in arr)
+                {
+                    if (setArr.Contains(target - item0 - item1))
+                    {
+                        Console.WriteLine($"{target - item0 - item1} {item0} {item1} ");
+                        return;
+                    }   
+                }
+                setArr.Add(item0);
+            }
+            Console.WriteLine("Число не найдено");
+        } 
+    }
+}
---
+++ ArrayNumbersDz4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ArrayNumbersDz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { -2, -4, -6, -12, 5, 9, 12, 16, 22, 2, 7, 1, 24, 35, -18, -8, 4 };
+            int target = 4;
+
+            ArrayNumberFind FindArray = new ArrayNumberFind();
+            FindArray.FindTarget(target, arr);
+        }
+    }
+}
